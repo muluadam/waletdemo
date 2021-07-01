0a3c5e08c4a9646a3fddead8793cdf845e4666da
muluadam <muluadamt@gmail.com>
2021-07-01 18:51:35 -0500

    test two is added

---
+++ src/main/java/com/cloud/waletdemo/TestController.java
@@ -0,0 +1,12 @@
+package com.cloud.waletdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test2")
+    public String testTwo(){
+        return "Test Two";
+    }
+}
